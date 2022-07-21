32471103c9e55ea20186e4d8ed3a0e439acde8b6
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-21 17:22:55 +0100

    users controllers

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,42 +1,42 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
-// const User = require('../models/user');
+const User = require('../models/user');
 
-// // router.get('/', async (req, res) => {
-// //     try {
-// //         const users = await User.all
-// //         res.status(200).json(users)
-// //     } catch (err) {
-// //         res.status(500).json({err})
-// //     }
-// // })
-// async function user (req, res) {
+// router.get('/', async (req, res) => {
 //     try {
 //         const users = await User.all
 //         res.status(200).json(users)
 //     } catch (err) {
 //         res.status(500).json({err})
 //     }
-// }
+// })
+async function user (req, res) {
+    try {
+        const users = await User.all
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({err})
+    }
+}
 
-// // router.get('/:email', async (req, res) => {
-// //     try {
-// //         const users = await User.findByEmail(req.params.email)
-// //         res.json(users)
-// //     } catch(err) {
-// //         res.status(500).json({err})
-// //     }
-// // })
-// async function userEmail (req, res) {
+// router.get('/:email', async (req, res) => {
 //     try {
 //         const users = await User.findByEmail(req.params.email)
 //         res.json(users)
 //     } catch(err) {
 //         res.status(500).json({err})
 //     }
-// }
+// })
+async function userEmail (req, res) {
+    try {
+        const users = await User.findByEmail(req.params.email)
+        res.json(users)
+    } catch(err) {
+        res.status(500).json({err})
+    }
+}
 
 
-// module.exports = { user, userEmail }
+module.exports = { user, userEmail }
