b63acffec15115739f7a8e9798e6e74172b82b2d
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-21 14:16:09 +0100

    moving files to test

--- api/server.js
+++ api/server.js
@@ -8,12 +8,13 @@ server.use(express.json())
 
 //routes
 const habitRoutes = require('./routes/habits')
-const authRoutes = require('./controllers/auth')
-const userRoutes = require('./controllers/users')
+// const authRoutes = require('./controllers/auth')
+// const userRoutes = require('./controllers/users')
 //
 server.use('/habits', habitRoutes)
-server.use('/auth', authRoutes )
-server.use('/user', userRoutes)
+// server.use('/auth', authRoutes)
+// server.use('/user', userRoutes)
 server.get('/', (req, res) => res.send('Hello, dojo!'))
+
 
 module.exports = server
