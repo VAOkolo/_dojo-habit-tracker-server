b85ab19d36871a91c7f653e67f4234382894c872
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-21 17:09:29 +0100

    adding users

---
+++ api/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/users')
+
+router.get('/', userController.user)
+router.get('/', userController.userEmail)
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -9,11 +9,11 @@ server.use(express.json())
 //routes
 const habitRoutes = require('./routes/habits')
 // const authRoutes = require('./controllers/auth')
-// const userRoutes = require('./controllers/users')
+const userRoutes = require('./routes/users')
 //
 server.use('/habits', habitRoutes)
 // server.use('/auth', authRoutes )
-// server.use('/user', userRoutes)
+server.use('/user', userRoutes)
 server.get('/', (req, res) => res.send('Hello, dojo!'))
 
 module.exports = server
