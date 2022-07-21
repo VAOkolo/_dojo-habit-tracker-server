1c97277f73df97c43ad054b5f77d491145807452
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-21 16:44:48 +0100

    adding files

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+//routes
+const habitRoutes = require('./routes/habits')
+// const authRoutes = require('./controllers/auth')
+// const userRoutes = require('./controllers/users')
+//
+server.use('/habits', habitRoutes)
+// server.use('/auth', authRoutes )
+// server.use('/user', userRoutes)
+server.get('/', (req, res) => res.send('Hello, dojo!'))
+
+module.exports = server
