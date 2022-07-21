b0b51f09ed3f47daef2f3b4f4051d7386974ede2
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-21 15:42:34 +0100

    adding async funcs to habits

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const cors = require('cors')
 
 const server = express()
@@ -6,15 +7,13 @@ server.use(cors())
 server.use(express.json())
 
 //routes
-
 const habitRoutes = require('./routes/habits')
 // const authRoutes = require('./controllers/auth')
 // const userRoutes = require('./controllers/users')
-// //
+//
 server.use('/habits', habitRoutes)
 // server.use('/auth', authRoutes )
 // server.use('/user', userRoutes)
-
 server.get('/', (req, res) => res.send('Hello, dojo!'))
 
 module.exports = server
